14d37699086be9dcea7f93919a03ef148b0e3399
Nurlan <78497823+nurl8n@users.noreply.github.com>
2021-02-24 05:23:14 +0600

    Add files via upload

---
+++ LAB4/src/app/products.ts
@@ -0,0 +1,101 @@
+export const products = [
+  {
+    id: 1,
+    url: 'https://www.amazon.com/PICTEK-Programmable-Breathing-Ergonomic-Computer/dp/B01FZ3BR5S/?_encoding=UTF8&pd_rd_w=pEYEO&pf_rd_p=2dbfe895-70fc-42d0-948e-36d5f770d93b&pf_rd_r=M4CM6SNW4FBMSQ7DD2JB&pd_rd_r=b66ab6b5-bb61-4af3-baa9-d6ac2480f275&pd_rd_wg=yQxJ7&ref_=pd_gw_unk',
+    name: 'PICTEK Gaming Mouse',
+    rating: 4.5,
+    price: 16.99,
+    description: 'PICTEK Gaming Mouse Wired Ergonomic Game USB Computer Mice RGB Gamer Desktop Laptop PC Gaming',
+    image: "https://images-na.ssl-images-amazon.com/images/I/61gxUsLtlyL.__AC_SX300_SY300_QL70_FMwebp_.jpg"
+  },
+  {
+    id: 2,
+    url: 'https://www.amazon.com/AMD-Ryzen-3600-12-Thread-Processor/dp/B07STGGQ18/ref=sr_1_1?dchild=1&keywords=processor&qid=1614118820&sr=8-1',
+    name: 'AMD Ryzen 5',
+    rating: 4.9,
+    price: 199.99,
+    description: 'AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor',
+    image: "https://images-na.ssl-images-amazon.com/images/I/71WPGXQLcLL._AC_SL1384_.jpg"
+  },
+  {
+    id: 3,
+    url:"https://www.amazon.com/Acer-i5-10300H-GeForce-Keyboard-AN515-55-59KS/dp/B08H2H89K1/ref=sr_1_2?dchild=1&keywords=laptop&qid=1614118399&sr=8-2",
+    name: 'Acer Nitro 5',
+    rating: 4.6,
+    price: 735.00,
+    description: 'Acer Nitro 5 Gaming Laptop, 10th Gen Intel Core i5-10300H,NVIDIA GeForce GTX',
+    image:"https://images-na.ssl-images-amazon.com/images/I/81Z8NvO2TFL._AC_SL1500_.jpg"
+  },
+  {
+    id: 4,
+    url:"https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=sr_1_2?dchild=1&keywords=monitor&qid=1614118674&sr=8-2",
+    name: 'Acer Zero Frame Monitor',
+    rating: 4.7,
+    price: 93.99,
+    description: 'Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor',
+    image:"https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg"
+  },
+  {
+    id: 5,
+    url:"https://www.amazon.com/Samsung-SM-G998B-Physical-Unlocked-International/dp/B08QTZ4JMS/ref=sr_1_2?dchild=1&keywords=samsung+s21&qid=1614119020&sr=8-2",
+    name: 'Samsung Galaxy S21 Ultra 5G',
+    rating: 5.0,
+    price: 1399.00,
+    description: 'Samsung Galaxy S21 Ultra 5G SM-G998B/DS Dual Physical Sim + Esim 16GB Ram, 512GB Storage GSM Factory Unlocked International Version - Phantom Silver',
+    image:"https://images-na.ssl-images-amazon.com/images/I/81ZJ%2BEMOKsL._AC_SL1500_.jpg"
+  },
+  {
+    id: 6,
+    url:"https://www.amazon.com/Corsair-Vengeance-2x16GB-PC4-28800-Desktop/dp/B08SPYCCF1/ref=sr_1_4?dchild=1&keywords=computer+memory&qid=1614119107&sr=8-4",
+    name: 'Corsair Vengeance Desktop Memory - Black',
+    rating: 5.0,
+    price: 190.00,
+    description: 'Corsair Vengeance RGB Pro SL 32GB (2x16GB) DDR4 3600 (PC4-28800) C18 1.35V Desktop Memory - Black (CMH32GX4M2D3600C18)',
+    image:"https://images-na.ssl-images-amazon.com/images/I/91wc8%2BM4jtL._AC_SL1500_.jpg"
+  },
+  {
+    id: 7,
+    url:"https://www.amazon.com/HP-N9K27AN-140-Original-Cartridges/dp/B01AV8PPOQ/ref=sr_1_21?dchild=1&qid=1614020759&s=computers-intl-ship&sr=1-21",
+    name: 'HP 952 Cartridges',
+    rating: 4.8,
+    price: 68.89,
+    description: 'HP 952 | 3 Ink Cartridges | Cyan, Magenta, Yellow | L0S49AN, L0S52AN, L0S55AN',
+    image:"https://images-na.ssl-images-amazon.com/images/I/710GvhofGwL._AC_SL1500_.jpg"
+  },
+  {
+    id: 8,
+    url:"https://www.amazon.com/Beginning-Angular-Typescript-updated/dp/1973593793/ref=sr_1_2?dchild=1&keywords=angular+book&qid=1614120057&sr=8-2",
+    name: 'Beginning Angular with Typescript',
+    rating: 4.0,
+    price: 16.99,
+    description: 'Beginning Angular with Typescript (updated to Angular 9)',
+    image:"https://images-na.ssl-images-amazon.com/images/I/41LYpNt7+WL._SX404_BO1,204,203,200_.jpg"
+  },
+  {
+    id: 9,
+    url:"https://www.amazon.com/dp/B0167H31CI/ref=s9_acsd_al_bw_c2_x_1_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-4&pf_rd_r=AMTDT59CXXS6M7VP76T6&pf_rd_t=101&pf_rd_p=a93fef9e-e2e8-4853-af0c-a24e77f0be1e&pf_rd_i=21217035011",
+    name: 'Philips Hue LightStrip',
+    rating: 4.6,
+    price: 69.99,
+    description: 'Philips Hue LightStrip Plus Dimmable LED Smart Light Extension (Requires Lightstrip Base & Hue Hub, Works with Alexa, HomeKit & Google Assistant)',
+    image:"https://images-na.ssl-images-amazon.com/images/I/8165pe%2B%2BLgL._AC_SL1500_.jpg"
+  },
+  {
+    id: 10,
+    url:"https://www.amazon.com/Xerox-VersaLink-Monochrome-Printer-Replenishment/dp/B01N4F39CB/ref=sr_1_1_sspa?dchild=1&keywords=printer&qid=1614119201&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzN0xGSDZUQ01SWVBNJmVuY3J5cHRlZElkPUEwMjMxMTczMUFBMlhUQVNSSDlYNiZlbmNyeXB0ZWRBZElkPUEwODc3NDgxMTdBTUZRRlQ0U05KNCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=",
+    name: 'Xerox VersaLink',
+    rating: 3.8,
+    price: 398.15,
+    description: 'Xerox VersaLink B400/DN Monochrome Printer, Amazon Dash Replenishment Ready',
+    image:"https://images-na.ssl-images-amazon.com/images/I/61m7XProiCL._AC_SL1215_.jpg"
+  },
+  {
+    id: 11,
+    url:"https://www.amazon.com/Angular-Enterprise-Ready-Web-Applications-production-grade/dp/1838648801/ref=sr_1_2?dchild=1&keywords=angular+introduction&qid=1614119361&s=books&sr=1-2",
+    name: 'Angular for Enterprise-Ready Web Applications',
+    rating: 4.4,
+    price: 39.99,
+    description: "Angular for Enterprise-Ready Web Applications: Build and deliver production-grade and cloud-scale evergreen web apps with Angular 9 and beyond, 2nd Edition",
+    image:"https://images-na.ssl-images-amazon.com/images/I/51O5MbvmHJL._SX404_BO1,204,203,200_.jpg"
+  }
+];
